3e70b98fea5167d6d05bfc4273ae1ed0f1f7055d
Matthew Tang <matthew.tang99@gmail.com>
2025-11-01 14:58:55 +0000

    Update PCA functions. Clarify variable names. Correct col names from PC1 to PC1 reduced data.

--- src/smlm_clust_analysis/internals/pca.py
+++ src/smlm_clust_analysis/internals/pca.py
@@ -3,9 +3,11 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-def extract_cluster_features(cluster_data: 'np.ndarray') -> 'np.ndarray':
+def extract_features_asarray(cluster_data: "pd.DataFrame") -> 'np.ndarray':
     
-    return cluster_data[:, 2:-1]
+    clust_features = cluster_data[cluster_data.columns[:-1]]
+    
+    return np.array(clust_features).astype(np.float32)
 
 def z_normalize(data: 'np.ndarray') -> 'np.ndarray':
     
@@ -23,15 +25,21 @@ def z_norm_cluster_features(cluster_features: 'np.ndarray') -> 'np.ndarray':
     
     return norm_data
 
-def pca(norm_features: 'np.ndarray') -> 'np.ndarray':
+def pca(norm_features: 'np.ndarray') -> tuple['np.ndarray']:
+
+    """
+    Carries out principal component analysis on normalized cluster features.
+    The function reduces the dimensionality of the cluster data and returns the
+    loadings of the first two principal components.
+    """
     
-    pca = PCA(n_components=3)
+    pca = PCA(n_components=2)
     
-    principalComponents = pca.fit_transform(norm_features)
+    data_pca = pca.fit_transform(norm_features)
     
-    #loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+    loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
     
-    return principalComponents
+    return data_pca, loadings
 
 def convert_to_df_2d(principal_components: 'np.ndarray') -> 'pd.DataFrame':
     
@@ -41,7 +49,7 @@ def convert_to_df_2d(principal_components: 'np.ndarray') -> 'pd.DataFrame':
                          'final dimensions.')
     
     principal_df = pd.DataFrame(data=principal_components,
-                                columns=['Principal component 1', 'Principal component 2'])
+                                columns=['PC1 Reduced Data', 'PC2 Reduced Data'])
     
     return principal_df
 
@@ -60,9 +68,9 @@ def convert_to_df_3d(principal_components: 'np.ndarray') -> 'pd.DataFrame':
     
     return principal_df
 
-def generate_final_df(principal_df: 'pd.DataFrame', original_df: 'pd.DataFrame') -> 'pd.DataFrame':
+def generate_final_df(reduced_df: 'pd.DataFrame', original_df: 'pd.DataFrame') -> 'pd.DataFrame':
     
-    final_df = pd.concat([principal_df, original_df[original_df.columns[-3:]]])
+    final_df = pd.concat([reduced_df, original_df], axis=1)
     
     return final_df
 
@@ -71,17 +79,17 @@ def plot_components_2d(final_df: 'pd.DataFrame') -> None:
     
     fig, ax = plt.subplots()
     
-    targets = set(final_df[final_df.columns[-1]])
+    conditions = set(final_df[final_df.columns[-1]])
     
     colors = iter(plt.cm.viridis(np.linspace(0, 1, final_df.shape[1])))
     
-    for target in targets:
+    for condition in conditions:
         
-        indices = final_df[final_df.columns[-1]] == target
+        indices = final_df[final_df.columns[-1]] == condition
         
-        ax.scatter(final_df.loc[indices, 'Principal component 1'], 
-                   final_df.loc[indices, 'Principal component 2'],
-                   c=next(colors), s=80, label=target)
+        ax.scatter(final_df.loc[indices, 'PC1 Reduced Data'], 
+                   final_df.loc[indices, 'PC2 Reduced Data'],
+                   c=next(colors), s=80, label=condition)
         
 
 def plot_components_3d(final_df: 'pd.DataFrame'):
